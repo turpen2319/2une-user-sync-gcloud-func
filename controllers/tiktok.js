27b8a31b4f9c35d8b7f9d4b20f213b0c9654fd53
turpen2319 <austin-turpen@outlook.com>
2022-07-31 17:16:50 -0500

    tests tiktok video upload

--- controllers/tiktok.js
+++ controllers/tiktok.js
@@ -1,7 +1,10 @@
 const axios = require('axios').default;
+const FormData = require('form-data');
+const fs = require('fs/promises');
 
-// const PROVIDER = 'tiktok'; //idk if this is correct...pretty sure it is
-const PROVIDER = 'oauth_google'; 
+
+const PROVIDER = 'oauth_tiktok'; //idk if this is correct...pretty sure it is
+// const PROVIDER = 'oauth_google'; 
 
 module.exports = {
     uploadVideo
@@ -9,17 +12,39 @@ module.exports = {
 
 async function uploadVideo(req, res) {
     const userId = req.params.userId;
-
+    
     const accessToken = await getAccessToken(userId);
+    const openId = await getTikTokOpenId(userId);
 
-    res.send(accessToken);
+    console.log({accessToken, openId})
+    
+
+    const video = await fs.readFile(process.env.HOME + '/Desktop/test1.mp4');
+    const endpoint = `https://open-api.tiktok.com/share/video/upload/?access_token=${accessToken}&open_id=${openId}`
+
+    const form = new FormData();
+    form.append('video', video, 'test1.mp4');
+    try {
+        const uploadResponse = await axios.post(endpoint, form, {
+            headers: {
+              ...form.getHeaders(),
+            },
+            maxContentLength: 100000000,
+            maxBodyLength: 100000000
+        });
+        console.log("upload ---> \n\n", uploadResponse.data);
+    
+        res.send(uploadResponse.data);
+        
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+    
 }
 
 async function getAccessToken(userId) {
     const endpoint = `https://api.clerk.dev/v1/users/${userId}/oauth_access_tokens/${PROVIDER}` //injection risk?
-
-     
-    // const endpoint = `https://api.clerk.dev/v1/users/user_2Bs2a7O8IswylxJXK6nXVwUEUYz`//for fetching a user by id
     
     try {
         const response = await axios({
@@ -36,8 +61,31 @@ async function getAccessToken(userId) {
         return response.data[0].token;
 
     } catch (error) {
-        console.log(error);
         return error;
     }
 
+}
+
+
+async function getTikTokOpenId(userId) {
+    const endpoint = `https://api.clerk.dev/v1/users/${userId}`//for fetching a user by id
+    
+    try {
+        const response = await axios({
+            method: "GET",
+            url: endpoint,
+            headers: {
+                "Authorization": `Bearer ${process.env.CLERK_API_KEY}`,
+                "Content-type": "application/json"
+            }
+        });
+
+        const clerkUserObj = response.data;
+        const userTikTokOpenId = clerkUserObj.external_accounts[0].provider_user_id; //this will need to change if we allow users to connect to other external accounts
+        
+        return userTikTokOpenId;
+
+    } catch (error) {
+        return error;
+    }
 }
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 //middle ware
 app.use(cors());
 
+app.use(express.json({limit: '50mb'})); //might need to increase this
+
 app.use('/clerk/webhooks/users', require('./routes/users'));
 app.use('/tiktok', require('./routes/tiktok'));
 
